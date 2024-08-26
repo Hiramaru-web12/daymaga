0c5e397766df2bda1d68fbfe7a8fdc08f1e8e9ff
Hiramaru-web12 <meg96.mcr2st@gmail.com>
2024-08-26 16:21:55 +0900

    新着情報セクション作成

--- index.php
+++ index.php
@@ -31,10 +31,14 @@
               <time class="p-card__time" datetime="<?php the_time('c'); ?>"><?php the_time('Y/n/j'); ?></time>
               <h1 class="p-card__title p-fv-card__title"><?php the_title(); ?></h1>
               <?php
-                $category = get_the_category();
-                if($category[0]) : ?>
-              <p class="p-card__category p-fv-card__category"><?php echo $category[0]->cat_name; ?></p>
-              <?php endif; ?>
+                $this_category = get_the_category();
+                if($this_category) {
+                $this_category_color = get_field( 'color', 'category_' . $this_category[0]->term_id); 
+                $this_category_name  = $this_category[0]->name;
+                echo '<p class="p-card__category p-fv-card__category" style= "' . esc_attr(
+                   'color:' . $this_category_color . '; 
+                   border: 1px solid ' . $this_category_color) . ';">' . esc_html( $this_category_name ) . '</p>';
+                }?>
             </div>
           </div>
           <?php $post_tags = get_the_tags(); ?>
@@ -63,6 +67,65 @@
     <div class="swiper-pagination p-fv-swiper__pagination"></div>
   </div>
 </section>
+
+<!-- 最新情報 -->
+<section class="l-latest p-latest">
+  <div class="l-latest__inner l-inner">
+    <h1 class="c-section__title p-latest__title"><img
+        src="<?php echo get_template_directory_uri(  ); ?>/img/common/logo.png" alt="ロゴ">新着情報</h1>
+    <div class="p-latest-card__wrap">
+      <?php $latest_query = new WP_Query(
+          array(
+            'post_type'      => 'post',
+            'posts_per_page' => 3
+          )
+        );
+        ?>
+      <?php if ($latest_query->have_posts()) : ?>
+      <?php while($latest_query->have_posts()) : ?>
+      <?php $latest_query->the_post(); ?>
+      <div class="p-card">
+        <a href="<?php the_permalink(); ?>"></a>
+        <div class="p-card__img">
+          <?php if (has_post_thumbnail()) : ?>
+          <?php the_post_thumbnail(); ?>
+          <?php else : ?>
+          <img src="<?php echo get_template_directory_uri(); ?>/img/common/noimg.png" alt="画像がありません">
+          <?php endif; ?>
+          <div class="p-card__body">
+            <time class="p-card__time" datetime="<?php the_time('c'); ?>"><?php the_time('Y/n/j'); ?></time>
+            <h1 class="p-card__title"><?php the_title(); ?></h1>
+            <?php
+                  $this_category = get_the_category();
+                  if($this_category) {
+                  $this_category_color = get_field( 'color', 'category_' . $this_category[0]->term_id);
+                  $this_category_name  = $this_category[0]->name;
+                  echo '<p class="p-card__category" style= "' . esc_attr(
+                     'color:' . $this_category_color . ';
+                     border: 1px solid ' . $this_category_color) . ';">' . esc_html( $this_category_name ) . '</p>';
+                  }?>
+          </div>
+        </div>
+        <?php $post_tags = get_the_tags(); ?>
+        <ul class="p-card-tag__list">
+          <?php if ($post_tags) : ?>
+          <?php foreach($post_tags as $tag) : ?>
+          <li class="p-card__tag c-tag"><a
+              href="<?php echo get_tag_link($tag->term_id); ?>"><?php echo $tag->name; ?></a>
+          </li>
+          <?php endforeach; ?>
+          <?php endif; ?>
+        </ul>
+      </div>
+      <?php endwhile; ?>
+      <?php endif; ?>
+      <?php wp_reset_postdata(); ?>
+    </div>
+    <div class="p-latest-button__wrap">
+      <a href="" class="c-more__button">もっとみる</a>
+    </div>
+</section>
+
 
 <?php get_template_part('template-parts/tag-list');?>
 
