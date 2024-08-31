c3ad2984a4feef803972543b81d575e272a909fe
Hiramaru-web12 <meg96.mcr2st@gmail.com>
2024-08-31 11:57:39 +0900

    すべての記事ページ作成

---
+++ template-parts/pagination.php
@@ -0,0 +1,18 @@
+<?php if(paginate_links()) : //ページが1ページ以上あれば以下を表示 ?>
+<!-- pagination -->
+<nav class="l-pagiNav pagination l-inner">
+  <div class="nav-links">
+    <?php
+          echo paginate_links(
+           array(
+            'end_size' => 1,
+            'mid_size' => 1,
+            'prev_next' => true,
+            'prev_text' => '<img src="' . esc_url(get_template_directory_uri() . '/img/common/prev-arrow.svg') . '" alt="前へ">',
+            'next_text' => '<img src="' . esc_url(get_template_directory_uri() . '/img/common/next-arrow.svg') . '" alt="次へ">',
+           )
+          );
+         ?>
+  </div>
+</nav><!-- /pagination -->
+<?php endif; ?>
