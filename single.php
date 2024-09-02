4a82f8c72721b6769cb6ab876abe27b4a81b0e0e
Hiramaru-web12 <meg96.mcr2st@gmail.com>
2024-09-02 17:09:36 +0900

    コードの整形

--- single.php
+++ single.php
@@ -5,30 +5,30 @@
     <article class="p-entry">
       <div class="p-entry__header">
         <div class="p-entry__meta">
-          <time class="p-entry__published" datetime="<?php the_time('c'); ?>"><span>公開日</span>
-            <?php the_time('Y/n/j'); ?></time>
+          <time class="p-entry__published" datetime="<?php the_time('c'); ?>">
+            <span>公開日</span>
+            <?php the_time('Y/n/j'); ?>
+          </time>
         </div>
         <div class="p-entry__category">
           <?php
-            $this_category = get_the_category();
-             if($this_category) {
-              $this_category_color = get_field( 'color', 'category_' . $this_category[0]->term_id); 
-               $this_category_name  = $this_category[0]->name;
-                echo '<p class="p-fv-card__category" style= "' . esc_attr(
-                   'color:' . $this_category_color . '; 
-                   border: 1px solid ' . $this_category_color) . ';">' . esc_html( $this_category_name ) . '</p>';
-                }?>
+          $this_category = get_the_category();
+          if ($this_category) {
+            $this_category_color = get_field('color', 'category_' . $this_category[0]->term_id);
+            $this_category_name = $this_category[0]->name;
+            echo '<p class="p-fv-card__category" style="' . esc_attr('color:' . $this_category_color . '; border: 1px solid ' . $this_category_color) . ';">' . esc_html($this_category_name) . '</p>';
+          }
+          ?>
         </div>
         <h1 class="p-entry__title">
           <?php the_title(); ?>
         </h1>
-        <?php if(has_post_thumbnail()):?>
-
+        <?php if (has_post_thumbnail()): ?>
         <div class="p-entry__img">
+          <?php the_post_thumbnail(); ?>
         </div>
-        <?php the_post_thumbnail(); ?>
         <?php else: ?>
-        <img src="<?php echo get_template_directory_uri(); ?>/img/noimg.png" alt="">
+        <img src="<?php echo esc_url(get_template_directory_uri() . '/img/noimg.png'); ?>" alt="No Image Available">
         <?php endif; ?>
       </div>
 
@@ -39,26 +39,21 @@
       <div class="p-entry__bottom">
         <p>この記事のタグ</p>
         <?php
-    $post_tags = get_the_tags();
-    if ($post_tags) {
-        echo '<div class="p-card-tag__list">';
-        foreach($post_tags as $tag) {
-            echo '<a href="' . get_tag_link($tag->term_id) . '" class="c-tag">' . $tag->name . '</a> ';
+        $post_tags = get_the_tags();
+        if ($post_tags) {
+          echo '<div class="p-card-tag__list">';
+          foreach ($post_tags as $tag) {
+            echo '<a href="' . esc_url(get_tag_link($tag->term_id)) . '" class="c-tag">' . esc_html($tag->name) . '</a> ';
+          }
+          echo '</div>';
         }
-        echo '</div>';
-    }
-    ?>
+        ?>
       </div>
     </article>
-
   </div>
-
 </section>
 
-
-
 <?php get_template_part('template-parts/entry-recommend'); ?>
-
 <?php get_template_part('template-parts/tag-list'); ?>
 <?php get_template_part('template-parts/cta'); ?>
 <?php get_footer(); ?>
