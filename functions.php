23709501267611a677ae55f0d2380d196c38e4b5
Hiramaru-web12 <meg96.mcr2st@gmail.com>
2024-09-23 16:53:54 +0900

    コンタクトフォーム作成

--- functions.php
+++ functions.php
@@ -23,4 +23,10 @@ function my_script_init() {
   return $title;
 }
 add_filter('the_title', 'custom_title_length');
+
+// Contact Form 7で自動挿入されるPタグ、brタグを削除
+add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+function wpcf7_autop_return_false() {
+  return false;
+} 
  ?>
